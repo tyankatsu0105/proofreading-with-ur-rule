f3f1894fea34363ecdcf889985c161a42cfd07c0
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2023-09-11 10:54:04 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,49 @@
 import "./App.css";
 
+import { TextlintKernel, TextlintKernelOptions } from "@textlint/kernel";
+
+const kernel = new TextlintKernel();
+const setup = async () => {
+  const options: TextlintKernelOptions = {
+    ext: ".txt",
+    // plugins: [
+    //   {
+    //     pluginId: "text",
+    //     plugin: await import("@textlint/textlint-plugin-text"),
+    //   },
+    // ],
+    // rules: [
+    //   {
+    //     ruleId: "no-todo",
+    //     rule: await import("textlint-rule-no-todo"),
+    //   },
+    // ],
+  };
+};
+
 function App() {
+  const options: TextlintKernelOptions = {
+    ext: ".txt",
+    plugins: [
+      {
+        pluginId: "text",
+        plugin: require("@textlint/textlint-plugin-text"),
+      },
+    ],
+    rules: [
+      {
+        ruleId: "no-todo",
+        rule: require("textlint-rule-no-todo"),
+      },
+    ],
+  };
+  kernel.lintText("TODO: text", options).then((result) => {
+    console.log({ result });
+    // assert.ok(typeof result.filePath === "string");
+    // assert.ok(result.messages.length === 1);
+  });
+
+  console.log({ kernel });
   return (
     <>
       <p>hello</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solid(), commonjs()],
+  define: {
+    "process.env": {},
+  },
+});
